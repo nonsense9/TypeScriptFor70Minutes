392330da2b83032e5cf851b303d9039bdd7a42cf
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-03 12:21:17 +0300

    Types, never, tuple

---
+++ Types.ts
@@ -0,0 +1,37 @@
+// Basic Types
+// let str: string = "Hello";
+// console.log(str);
+
+// const int:number = 42;
+// let int1:number = 1.2;
+// const int2: number = 3e10;
+
+// console.log(int2);
+
+// const Arr1: number[] = [1, 2, 3];
+// const Arr2: Array<number> = [1, 2, 3];
+
+// // Tuple
+// const cont: [string,  number] = ['vlad', 123456];
+
+// function sayMyName (name: string): string {
+//   console.log(name);
+//   return name;
+// }
+// sayMyName('Alex');
+
+// // Never
+
+// function throwError(message: string): never {
+//   throw new Error(message); 
+// }
+// throwError('Erorrrrrr');
+
+
+// Type
+
+// type ID = number;
+// const id1: ID = parseInt("123") + 1;
+// console.log(id1);
+
+// type SomeType = string | null | undefined;
